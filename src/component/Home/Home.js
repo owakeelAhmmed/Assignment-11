41c37f6fd3110decfacb8d9ce18b1ff3ef19a8c2
owakeelAhmmed <mdowakeelahmed5678@gmail.com>
2022-04-30 01:12:40 +0600

    Home Pages Done

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -6,9 +6,9 @@ const Home = () => {
         <div className='container'>
              <div className="row mt-5">
                 <div className="col-6 text-start mt-5">
-                    <h1>Welcome To Our MediHouse</h1>
+                    <h1 className="">Welcome To Our MediHouse</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium soluta voluptatum maxime eligendi molestiae ullam eaque voluptate necessitatibus magnam praesentium.</p>
-                    <button className="btn btn-primary rounded-pill">Buy Now</button>
+                    <button className="btn btn-primary rounded-pill">Buy Now</button>   
                 </div>
                 <div className="col-6">
                     <img className='img-fluid' src={img1} alt="" />
